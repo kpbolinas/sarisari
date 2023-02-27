d4b3ef715e18848cfce72ee86cd9f9e11380706f
KV <kv@KVs-MacBook-Pro.local>
2023-02-27 17:11:10 +0800

    removed debug code

--- resources/js/components/pages/Logout.vue
+++ resources/js/components/pages/Logout.vue
@@ -10,19 +10,12 @@ export default {
   name: "LogoutPage",
   async beforeCreate() {
     await UserApiService.logout()
-      .then(() => {
+      .then(() => {})
+      .catch(() => {})
+      .finally(() => {
         localStorage.clear();
         sessionStorage.clear();
         Router.push("/login");
-      })
-      .catch(({ response }) => {
-        if (response?.status === 401) {
-          localStorage.setItem("auth-info", "");
-          Router.push("/unauthorized");
-        } else {
-          const message = response.data?.message;
-          console.log(message);
-        }
       });
   },
 };
